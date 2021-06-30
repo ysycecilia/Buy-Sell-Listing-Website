ca37afc567f65d1e4e05541f332954f5949398f5
Senayson <senaialema@gmail.com>
2021-06-30 15:01:43 +0000

    Adjustment of scripts and links on home.ejs & listingBySeller.ejs

--- public/scripts/featureListings.js
+++ public/scripts/featureListings.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
   //Function creates listing element
   const createListingElement = function(listing) {
     const $listing = $(`<div class="card ml-1 mr-1" style="width: 300px;">
-  //  <div> <i class="far fa-heart" id='fav-button'> Add to Favourites</i> </div>
+    <i class="far fa-heart" data-listing="${listing.id}"
+    data-user="${listing.user_id}" data-id="far-fa-heart" id="fas-fa-heart"> Add to Favourites</i>
+
     <img src=${listing.cover_picture_url} class="card-img-top" alt="..." id="listing-image">
     <div class="card-body text-center">
         <h5 class="card-title">${listing.title}</h5>
 
-        <i class="far fa-heart" data-listing="${listing.id}"
-        data-user="${listing.user_id}" data-id="far-fa-heart" id="fas-fa-heart"></i>
         <h4 class="card-title">$${listing.price}</h4>
         <p class="card-text">${listing.description}</p>
         <a href="#" class="btn btn-primary">View Listing</a>
